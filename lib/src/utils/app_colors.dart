23539d73765601610e47eec17fff03cd22d7a4e8
guoganjohn <johnguogan@gmail.com>
2025-10-25 03:58:46 -0300

    fix: Adjust AppColors values

--- lib/src/utils/app_colors.dart
+++ lib/src/utils/app_colors.dart
@@ -5,9 +5,9 @@ class AppColors {
   // Prevent instantiation
   AppColors._();
 
-  static const Color primary = Color(0xFF039BE5); // Deep Sky Blue
-  static const Color secondary = Color(0xFFFFFFFF); // White
+  static const Color primary = Color(0xFF0d86cb); // Deep Sky Blue
+  static const Color secondary = Color(0xFFfbfefd); // White
   static const Color accent = Color(0xFF1E88E5); // Medium Blue
   static const Color text = Color(0xFF424242); // Greyish text
-  static const Color background = Color(0xFFF7F7F7); // Light background
+  static const Color background = Color(0xFFfbfefd); // Light background
 }
