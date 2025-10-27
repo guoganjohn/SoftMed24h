789ab8c09fb0638cd1559d5104c8601cdf843fa6
guoganjohn <johnguogan@gmail.com>
2025-10-27 14:18:34 -0300

    Confirm Rebuild and Deployment

--- softmed24h/flutter_service_worker.js
+++ softmed24h/flutter_service_worker.js
@@ -31,7 +31,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "4cbafadd128d2d6d4b40d3699f34afe1
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "a0c89304af9c7ac363e367b0fde85bd6",
+"flutter_bootstrap.js": "937a03d600018688cc90133cb0b01975",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
