9093914d1360f9c3aa52c09dda0889f6dbe1bdaf
guoganjohn <johnguogan@gmail.com>
2025-10-28 06:54:17 -0300

    build: Rebuild Flutter web application
    This commit includes the latest build of the Flutter web application.

--- dist/flutter_service_worker.js
+++ dist/flutter_service_worker.js
@@ -31,7 +31,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "9cbaabbcae917e9ec7741e75cf710551
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "0b6fac98fa8db60c678fa89ba58dca19",
+"flutter_bootstrap.js": "c663390d74d60a7e2249b28943586912",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
