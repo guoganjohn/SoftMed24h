f763d64646ddcd6d40d0f211bc8f8f465c4989eb
guoganjohn <johnguogan@gmail.com>
2025-10-28 11:59:04 -0300

    build: Rebuild Flutter web application after address and tooltip changes
    This commit includes the latest build of the Flutter web application after implementing CEP-based address lookup and adding tooltips to the register screen.

--- dist/flutter_service_worker.js
+++ dist/flutter_service_worker.js
@@ -31,14 +31,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "9cbaabbcae917e9ec7741e75cf710551
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "c663390d74d60a7e2249b28943586912",
+"flutter_bootstrap.js": "cebc2affed83e936cb3128f2a76206c4",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "1fb12bc3d954fbebbd3469c2fe189758",
 "/": "1fb12bc3d954fbebbd3469c2fe189758",
-"main.dart.js": "943331dd9cb8fd24f14163ef7d906bec",
+"main.dart.js": "cd5872fe1cc222c4b71b47eb04578739",
 "manifest.json": "3896b9e96b96ecc028df29f7a26e3dbb",
 "version.json": "ef2a079c7a948a12d2c555e5eef58aae"};
 // The application shell files that are downloaded before a service worker can
